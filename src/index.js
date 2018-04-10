90324a5244abe52547ce5dc100dd9b8b5f6197af
Ramgopal Bhat <subramanyaramgopal@gmail.com>
2018-04-10 12:14:52 +0530

    created layout and basic routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Miss, Match } from "react-router";
 
-import StorePicker from './components/StorePicker';
+import "./css/style.css";
 
-render(<StorePicker/>, document.querySelector("#main"));
+import StorePicker from "./components/StorePicker";
+import App from "./components/App";
+import NotFound from "./components/NotFound";
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match exactly pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  );
+};
+
+render(<Root />, document.querySelector("#main"));
